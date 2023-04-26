c7885fe042fdcb3dda124617d8a1ff0b137bb1e4
Trehan-Mayank <129321675+Trehan-Mayank@users.noreply.github.com>
2023-04-27 00:05:58 +0530

    Update TEST_VMC .v

--- TEST_VMC .v
+++ TEST_VMC .v
@@ -1,3 +1,25 @@
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Mayank Trehan
+// 
+// Create Date: 19.04.2023 11:03:22
+// Design Name: 
+// Module Name: TEST
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module TEST();
 
 reg clk,rst,Coin_01Rs,Coin_02Rs,Coin_05Rs,Coin_10Rs;
